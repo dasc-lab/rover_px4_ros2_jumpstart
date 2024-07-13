dc39cf99865a0e4d80804aefe02bb39b3647d1f2
alfang001 <albfang@umich.edu>
2024-07-13 17:41:40 -0400

    added new recorded data

--- colcon_ws/src/trajectory_driver/trajectory_driver/figureeight.py
+++ colcon_ws/src/trajectory_driver/trajectory_driver/figureeight.py
@@ -11,11 +11,11 @@ class figureeight(Node):
         super().__init__('figureeight')
 
         ###### set up circle parameters ######
-        self.radius = 0.4
+        self.radius = 0.3
         self.height = -0.4
-        self.center_x = 1.0
+        self.center_x = 0.0
         self.center_y = 0.0
-        self.angular_vel = 2.5
+        self.angular_vel = 1.5
 
 
         ###### set up node parameters ######
