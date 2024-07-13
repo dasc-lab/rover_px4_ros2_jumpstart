581e76fbcc557c9dcae43125c100e5d5a0c916a2
alfang001 <albfang@umich.edu>
2024-07-13 17:57:41 -0400

    added figure8 data

--- colcon_ws/src/trajectory_driver/launch/figure8.launch.py
+++ colcon_ws/src/trajectory_driver/launch/figure8.launch.py
@@ -6,7 +6,7 @@ def generate_launch_description():
     rosbag = ExecuteProcess(
             cmd=[
                 'ros2', 'bag', 'record',
-                '-o', 'eight_traj_r0.4_w2.5_c10_h0.4_fanhigh',
+                '-o', 'eight_traj_r0.4_w2.0_c10_h0.5_kxv74_unclipped',
                 '/drone/combined_data'
             ],
             output='screen'
