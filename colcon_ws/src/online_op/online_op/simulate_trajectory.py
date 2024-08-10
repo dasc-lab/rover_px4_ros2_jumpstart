d207816c8fb7625696b49f9bfa48a5a2eb437972
albus-fang <albusalbertfang@gmail.com>
2024-08-10 18:16:22 -0400

    Simulate Trajectory with simulate_trajectory.py to debug optimize.py

--- colcon_ws/src/online_op/online_op/simulate_trajectory.py
+++ colcon_ws/src/online_op/online_op/simulate_trajectory.py
@@ -21,32 +21,38 @@ class trajecotry(Node):
         self.ned_pos = None
         self.ned_vel = None
         self.ned_acc = None
-        
+        # self.initialize_pos_vel_acc()
+        # self.period = 2*3.1415926/1.0
         ############ set up publisher ############
         self.publisher_ = self.create_publisher(VehicleLocalPosition, '/px4_1/fmu/out/vehicle_local_position', 10)
-        
+        # self.timer =self.create_timer(1./60., self.timer_callback)
         
         ################## set up Subscription ##################
 
         #self.timer = self.create_timer(1./80., self.timer_callback)
+
         self.trajectory_data = self.create_subscription(
 		    CombinedData,
 		    '/drone/combined_data',
 		    self.coordinate_callback,
 		    10)
+
             # qos_profile=qos_profile)
         
-        # self.ref = self.create_subscription(
-		#     TrajectorySetpoint,
-		#     '/px4_1/fmu/in/trajectory_setpoint',
-		#     self.reference_callback,
-		#     10)
+        
+    def initialize_pos_vel_acc(self):
+        self.ned_pos = np.load("dataset/pos_vec.npy")
+        self.ned_vel = np.load("dataset/vel_vec.npy")
+        self.ned_acc = np.load("dataset/acc_vec.npy") 
+    
     def coordinate_callback(self, msg):
+        print(type(msg.pos))
         self.ned_pos = msg.pos
         self.ned_vel = msg.vel
         self.ned_acc = msg.acc
         message = self.create_vehicle_msg()
         self.publisher_.publish(message)
+
     def create_vehicle_msg(self):
         msg = VehicleLocalPosition()
         msg.x, msg.y, msg.z = self.ned_pos
