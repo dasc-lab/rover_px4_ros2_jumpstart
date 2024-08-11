6db51698599a4979cf7df7442bc523cc2b9ba003
albus-fang <albusalbertfang@gmail.com>
2024-08-11 10:09:42 -0400

    bug fixes in optimize.py

--- colcon_ws/src/online_op/online_op/optimize.py
+++ colcon_ws/src/online_op/online_op/optimize.py
@@ -135,7 +135,7 @@ class optimizer(Node):
             self.current_vel = [msg.vx,msg.vy,msg.vz]
             self.current_state = jnp.array(self.current_pos + self.current_vel)
             print(self.current_state.shape)
-            assert(self.current_state.shape is (6,))
+            # assert(self.current_state.shape is (6,))
             if self.trajectory_type_valid is True:
                 deltaT = (self.get_clock().now().nanoseconds-self.start_time)/10**9
                 # ref_coord = self.find_ref_coord(deltaT)
