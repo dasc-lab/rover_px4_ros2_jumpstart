9f113a2795da89f2193cce8b859531ee677f1dfe
albus-fang <albusalbertfang@gmail.com>
2024-08-10 20:14:14 -0400

    bug fixes in simulate_trajectory.py

--- colcon_ws/src/online_op/online_op/simulate_trajectory.py
+++ colcon_ws/src/online_op/online_op/simulate_trajectory.py
@@ -57,9 +57,9 @@ class trajecotry(Node):
         msg = VehicleLocalPosition()
         # print(type(self.ned_pos))
         # print(type(self.ned_pos[0]))
-        msg.x, msg.y, msg.z = float(ned_pos[0]), ned_pos[1], ned_pos[2]
-        msg.vx, msg.vy, msg.vz = ned_vel[0], ned_vel[1], ned_vel[2]
-        msg.ax, msg.ay, msg.az = ned_acc[0], ned_acc[1], ned_acc[2]
+        msg.x, msg.y, msg.z = float(ned_pos[0]), float(ned_pos[1]), float(ned_pos[2])
+        msg.vx, msg.vy, msg.vz = float(ned_vel[0]), float(ned_vel[1]), float(ned_vel[2])
+        msg.ax, msg.ay, msg.az = float(ned_acc[0]), float(ned_acc[1]), float(ned_acc[2])
 
         return msg
     
