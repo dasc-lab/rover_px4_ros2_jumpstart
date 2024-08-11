4bce5f1d338522536db70f4854e3b311d8a4c4b3
albus-fang <albusalbertfang@gmail.com>
2024-08-11 15:06:38 -0400

    bug fixes in optimize.py

--- colcon_ws/src/online_op/online_op/optimize.py
+++ colcon_ws/src/online_op/online_op/optimize.py
@@ -126,7 +126,7 @@ class optimizer(Node):
     def trajectory_info_callback(self,msg):
         if self.trajectory_type_valid is False:
             self.trajectory_type = msg.type
-            self.trajectory_type_int = 0 if self.trajectory_type is 'circle' else 1
+            self.trajectory_type_int = 0 if self.trajectory_type == 'circle' else 1
             print(self.trajectory_type, self.trajectory_type is 'circle')
             self.radius = msg.radius
             self.angular_vel = msg.angular_vel
@@ -200,9 +200,10 @@ class optimizer(Node):
         print("policy params type is: ",type(params_policy))
         print("gp train type is: ",type(gp_train_x), type(gp_train_y))
         print("deltaT type is: ",type(deltaT))
+        ref_pos,ref_vel,ref_acc = self.find_ref_pos_vel_acc(deltaT)
         def body(i, inputs):
             params_policy = inputs
-            params_policy_grad = get_future_reward_grad( init_state, params_policy, [self.gp0, self.gp1, self.gp2], [self.sigma0, self.sigma1, self.sigma2], gp_train_x, gp_train_y, deltaT, self.horizon, self.trajectory_type_int, [self.radius, self.angular_vel, self.center_x, self.center_y])
+            params_policy_grad = get_future_reward_grad( init_state, params_policy, [self.gp0, self.gp1, self.gp2], [self.sigma0, self.sigma1, self.sigma2], gp_train_x, gp_train_y, deltaT, self.horizon)
             params_policy_grad = jnp.clip( params_policy_grad, -self.grad_clip, self.grad_clip )
             params_policy = params_policy - self.custom_lr_rate * params_policy_grad
             return params_policy
--- colcon_ws/src/online_op/online_op/optimize_helper.py
+++ colcon_ws/src/online_op/online_op/optimize_helper.py
@@ -31,7 +31,7 @@ def reward_func(states, weights, pos_ref, vel_ref):
     reward = pos_factor * jnp.sum(ex_ev_mean[0:3] ** 2) + vel_factor * jnp.sum(ex_ev_mean[3:6] ** 2)
     return reward
 @jit
-def get_future_reward(state, params_policy, gps, sigma_inv, gp_train_x, gp_train_y, deltaT, horizon, trajectory_type, trajectory_parameters):
+def get_future_reward(state, params_policy, gps, sigma_inv, gp_train_x, gp_train_y, deltaT, horizon, reference_pos_vel_acc):
     print("Calculating Reward")
     states,weights = initialize_sigma_points( state )
     w1 = 0.5
@@ -48,7 +48,8 @@ def get_future_reward(state, params_policy, gps, sigma_inv, gp_train_x, gp_train
         '''
         t = h * op_dt+ deltaT
         reward, states, weights = inputs
-        ref_pos, ref_vel, ref_acc = find_ref_pos_vel_acc(trajectory_type,t, trajectory_parameters)
+        # ref_pos, ref_vel, ref_acc = find_ref_pos_vel_acc(trajectory_type,t, trajectory_parameters)
+        ref_pos,ref_vel,ref_acc = reference_pos_vel_acc
         ###### fixed policy ######
         
         control_inputs, pos_ref, vel_ref = policy( state, params_policy, [ref_pos,ref_vel,ref_acc])         # mean_position = get_mean( states, weights )
@@ -67,7 +68,7 @@ get_future_reward_grad = jit(grad(get_future_reward, argnums=1))
 def find_ref_pos_vel_acc(trajectory_type, deltaT, parameters):
         radius, angular_vel, center_x, center_y = parameters
         print(f"radius: {radius}, ")
-        if trajectory_type == 0: #' circle'
+        if trajectory_type == 'circle': #' circle'
             pos_vel_acc = circle_pos_vel_acc
         else:
             pos_vel_acc = figure8_pos_vel_acc
