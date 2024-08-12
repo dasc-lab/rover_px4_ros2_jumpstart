f1f3a5042d75fbd148d95b5c36660d30120a62d2
albus-fang <albusalbertfang@gmail.com>
2024-08-11 21:39:29 -0400

    bug fixes in optimize.py

--- colcon_ws/src/online_op/online_op/optimize.py
+++ colcon_ws/src/online_op/online_op/optimize.py
@@ -98,8 +98,8 @@ class optimizer(Node):
         # w2 = 0.1
         
         # self.op_dt = 0.05
-        self.custom_lr_rate = 0.05
-        self.grad_clip = 100.0
+        self.custom_lr_rate = 0.1
+        self.grad_clip = 200.0
         self.iter_adam_custom = 200
         
         ###### set up mavlink ######
