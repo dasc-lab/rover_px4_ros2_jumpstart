4bea6cc97f380d55b082902929f80f7d1e2cc473
albus-fang <albusalbertfang@gmail.com>
2024-08-12 19:13:45 -0400

    bug fixes in optimize.py

--- colcon_ws/src/online_op/online_op/optimize.py
+++ colcon_ws/src/online_op/online_op/optimize.py
@@ -246,7 +246,10 @@ class optimizer(Node):
         return ref_pos,ref_vel,ref_acc
     def create_ParameterReq_msg(self, param_name_,value_):
         msg = ParameterReq()
-        msg.param_name = param_name_
+        param_name_char_array = ['']*16
+        for i in range(len(param_name_)):
+            param_name_char_array[i] = param_name_[i]
+        msg.param_name = param_name_char_array
         msg.set = True
         msg.value = value_
     def publish_gains(self):
