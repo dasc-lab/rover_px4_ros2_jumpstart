3b089eccd7653bba958ca17aed25c38eba8687c1
albus-fang <albusalbertfang@gmail.com>
2024-08-12 20:46:41 -0400

    bug fixes in optimize.py

--- colcon_ws/src/online_op/online_op/test_policy.py
+++ colcon_ws/src/online_op/online_op/test_policy.py
@@ -28,7 +28,7 @@ def policy( states, policy_params,reference):
     vel_ref = jnp.reshape(vel_ref, (-1,1))
     acc_ref = jnp.reshape(acc_ref, (-1,1))
     ex = states[0:3] - pos_ref
-    print("policy pos error is: ", ex.item())
+    print("policy pos error is: ", ex[0].item(), ex[1].item(), ex[2].item())
     # hcb.id_print(ex)
     # ex = lax.cond( jnp.linalg.norm(ex)>2, lambda z: 2.0 * z / jnp.linalg.norm(z), lambda z: z, ex )
     ev = states[3:6] - vel_ref
