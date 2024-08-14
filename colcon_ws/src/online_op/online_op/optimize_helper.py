457d2e55a17b0d4ea6e5540b519e088c69e3d78c
albus-fang <albusalbertfang@gmail.com>
2024-08-14 13:14:08 -0400

    added sparse gp models

--- colcon_ws/src/online_op/online_op/optimize_helper.py
+++ colcon_ws/src/online_op/online_op/optimize_helper.py
@@ -3,7 +3,7 @@ import jax
 import numpy as np
 from jax import grad, jit
 from .test_jax_utils import *
-from .test_gp_utils import get_next_states_with_gp_sigma_inv
+from .test_gp_utils_sparse import get_next_states_with_gp_sigma_inv
 from .test_policy import policy, circle_pos_vel_acc, figure8_pos_vel_acc
 from jax.experimental import host_callback as hcb
 
