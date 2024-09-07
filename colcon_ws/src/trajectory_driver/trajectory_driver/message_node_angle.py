f733f9fe5cc42b01972bea1d79976d38984fc564
albus-fang <albusalbertfang@gmail.com>
2024-09-06 21:14:43 -0400

    message node now publishes gains

--- colcon_ws/src/trajectory_driver/trajectory_driver/message_node_angle.py
+++ colcon_ws/src/trajectory_driver/trajectory_driver/message_node_angle.py
@@ -3,7 +3,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import *
 import numpy as np
-from px4_msgs.msg import TrajectorySetpoint, VehicleLocalPosition, VehicleAttitude, ParameterReq, ParameterRes
+from px4_msgs.msg import TrajectorySetpoint, VehicleLocalPosition, VehicleAttitude
 from foresee_msgs.msg import DynamicsData as CombinedData
 from geometry_msgs.msg import TransformStamped
 from rclpy.clock import Clock
@@ -33,16 +33,12 @@ class message(Node):
         self.acc_ref = None
         self.acc_com = None
         self.angles = None
-        self.kx = None
-        self.kv = None
         self.ref_valid = False
         self.angle_valid = False
-        self.gains_valid = False
         ############ set up publisher ############
         self.publisher_ = self.create_publisher(CombinedData, '/drone/combined_data', 10)
-        self.parameter_req_publisher_ = self.create_publisher(ParameterReq,'/px4_1/fmu/in/parameter_req',10)
-        ############ set up timer for gains ############
-        self.timer = self.create_timer(1.0/80.0, self.timer_callback)
+        
+    
         ################## set up Subscription ##################
 
         #self.timer = self.create_timer(1./80., self.timer_callback)
@@ -63,14 +59,7 @@ class message(Node):
 		    '/px4_1/fmu/out/vehicle_attitude',
 		    self.angle_callback,
 		    10)
-        self.gains = self.create_subscription(
-		    ParameterRes,
-		    '/px4_1/fmu/out/parameter_res',
-		    self.gains_callback,
-		    10)
         
-    def timer_callback(self):
-        self.request_gains()
     def create_CombinedData_msg(self):
         msg = CombinedData()
         msg.pos = self.ned_pos
@@ -80,43 +69,14 @@ class message(Node):
         msg.vel_ref = self.vel_ref
         msg.acc_ref = self.acc_ref
         msg.angle = self.angle
-        msg.kx = self.kx
-        msg.kv = self.kv
         return msg
         ################## set up call backs ##################
-    
-
-    def create_Parameter_Req_msg(self, param_name_, value_=0):
-        msg = ParameterReq()
-        param_name_char_array = ['']*16
-        # print("lenght of param_name_ is: ",len(param_name_))
-        for i in range(len(param_name_char_array)):
-            if i < len(param_name_):
-                param_name_char_array[i] = ord(param_name_[i])
-            else:
-                param_name_char_array[i] = ord('\0')
-        # print(type(param_name_char_array[0]))
-        # print("length of param_name_char_array is: ",len(param_name_char_array))
-        # param_name_ = param_name_.ljust(16, '\0')
-        msg.param_name = param_name_char_array
-        msg.set = False
-        # print("value_ is: ", value_)
-        # print("Type of value is: ", type(value_.item()))
-        msg.value = float(value_)
-        return msg
-    
-    def request_gains(self):
-        kx_msg = self.create_Parameter_Req_msg('QUAD_KX')
-        kv_msg = self.create_Parameter_Req_msg('QUAD_KV')
-        self.parameter_req_publisher_.publish(kx_msg)
-        self.parameter_req_publisher_.publish(kv_msg)
-
+        
     def coordinate_callback(self, msg):
         self.ned_pos = [msg.x,msg.y,msg.z]
         self.ned_vel = [msg.vx,msg.vy,msg.vz]
         self.ned_acc = [msg.ax, msg.ay, msg.az]
-        
-        if not self.ref_valid or not self.angle_valid or not self.gains_valid:
+        if not self.ref_valid or not self.angle_valid:
             return
         message = self.create_CombinedData_msg()
         self.publisher_.publish(message)
@@ -126,7 +86,7 @@ class message(Node):
         self.vel_ref = msg.velocity
         self.acc_ref = msg.acceleration
         self.ref_valid = True
-    
+    #def get_ground_truth_coord(self):
     
     def angle_callback(self, msg):
         quat= msg.q
@@ -135,15 +95,7 @@ class message(Node):
         roll, pitch, yaw = r.as_euler('xyz',degrees=True)
         self.angles = np.array([roll, pitch, yaw])
         self.angle_valid = True
-    
-    def gains_callback(self, msg):
-        param_name = msg.param_name
-        if param_name == 'QUAD_KX':
-            self.kx = msg.value
-        if param_name == 'QUAD_KV':
-            self.kv = msg.value
-        if self.kx is not None and self.kv is not None:
-            self.gains_valid = True
+
 def main(args=None):
     rclpy.init(args=args)
 
